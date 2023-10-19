90fd877e4baa3307007a83aa444c9b6d5b8b2f12
Ramani Muniyappan <ramanicsegnxt@gmail.com>
2023-10-19 22:21:53 +0530

    added new package and added AddTwoNumbers.java

---
+++ src/com/leetcode/problems/medium/AddTwoNumbers.java
@@ -0,0 +1,42 @@
+package com.leetcode.problems.medium;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// https://leetcode.com/problems/add-two-numbers
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int a = l1 != null ? l1.val : 0;
+            int b = l2 != null ? l2.val : 0;
+            int sum = a + b + carry;
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return head.next;
+    }
+}
--- src/com/leetcode/problems/medium/LongestSubstringWithoutRepeatingCharacters.java
+++ src/com/leetcode/problems/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,4 +1,4 @@
-package com.leetcode.problems.easy;
+package com.leetcode.problems.medium;
 
 import java.util.HashMap;
 
